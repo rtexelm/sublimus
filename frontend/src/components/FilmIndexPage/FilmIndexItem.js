03f0639d381a4d97a4486b64d5e4eb0306df4d0d
rtexelm <texelist@gmail.com>
2022-12-10 22:37:10 -0500

    Further stylings of a HTML table

--- frontend/src/components/FilmIndexPage/FilmIndexItem.js
+++ frontend/src/components/FilmIndexPage/FilmIndexItem.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import styles from "./films.module.scss";
 
 function FilmIndexItem({ film }) {
   const { id, title, director, year, price } = film;
+  const history = useHistory();
 
   return (
-    <tr>
-      <td>{id}</td>
-      <td></td>
-      <td className={`${styles.tableTitle}`}>
-        <Link to={`films/${film.id}`}>{title}</Link>
-      </td>
-      <td>{director}</td>
+    <tr onClick={(e) => history.push(`films/${film.id}`)}>
+      <td className={`${styles.tableSpine}`}>{id}</td>
+      <td className={`${styles.tableImg}`}></td>
+      <td className={`${styles.tableTitle}`}>{title}</td>
+      <td className={`${styles.tableDirector}`}>{director}</td>
       <td>{year}</td>
       <td>{price}</td>
     </tr>
