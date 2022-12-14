a82fdfa6c5c64d9d6004a9585acd081126ea2eda
rtexelm <texelist@gmail.com>
2022-12-14 12:30:10 -0500

    change migration

--- db/migrate/20221212230723_create_carts.rb
+++ db/migrate/20221212230723_create_carts.rb
@@ -1,8 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
+      t.references :cart_item, foreign_key: true
       t.timestamps
     end
-    add_reference :carts, :cart_item, index: true
   end
 end
