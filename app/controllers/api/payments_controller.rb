ab508ba18bb17d52d6c83526bef8b73d2190e0b8
rtexelm <texelist@gmail.com>
2023-02-04 22:06:41 -0500

    Secret certain things

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,10 +1,11 @@
 require 'stripe'
+require_relative '../../../.stripe_key.rb'
 
 class Api::PaymentsController < ApplicationController
   
   def create
 
-    Stripe.api_key = 'pk_test_51MXcBcKqBlU1u50lTAHi1JvNzQ3hwmbnlSP55z8IUYsrXyZj60HzAZXi2QkTMKco3HgaGZf6bVv7NZyisND49Kdi00qWdDoabt'
+    Stripe.api_key = $stripe_key
     
   end
   
