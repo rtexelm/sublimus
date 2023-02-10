9bbfa3eddf1c788af37f813833330fdd57f2e84d
rtexelm <texelist@gmail.com>
2023-02-09 20:04:49 -0500

    Expand payment form functions

--- frontend/src/components/CheckoutPage/PaymentForm.jsx
+++ frontend/src/components/CheckoutPage/PaymentForm.jsx
@@ -9,10 +9,44 @@ function PaymentForm() {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const { error } = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+    });
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      setErrorMessage(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
-      <button>Submit</button>
+      <button disabled={!stripe}>Submit</button>
+      {errorMessage && <div>{errorMessage}</div>}
     </form>
   );
 }
