e694b23aeb032b52d97d2287a3cd2f219466a92e
rtexelm <texelist@gmail.com>
2023-02-22 20:51:16 -0500

    Minor styling, begin Social Links page

---
+++ frontend/src/components/Navigation/SocialLInks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SocialLinks() {
+  const linkedIn = {};
+  const git = {};
+  const email = {};
+  const portfolio = {};
+  const angelList = {};
+}
+
+export default SocialLinks;
