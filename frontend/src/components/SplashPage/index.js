cf576ee71e1e8f39b050ee1ffa5393a0aede2e6d
rtexelm <texelist@gmail.com>
2023-02-23 00:56:34 -0500

    Add social links

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import SocialLinks from "./Social/SocialLInks";
 import styles from "./SplashPage.module.scss";
 
 function SplashPage() {
   return (
     <main>
+      <SocialLinks />
       <article className={`${styles.splashFeature} ${styles.spotlight}`}>
         <img
           src="https://s3.amazonaws.com/criterion-production/spotlight_images/4245-aa50a5f37415a5b31d01577f73327950/I7ltCM01T3eYLeC94Ag5Gd1zTTgcNP_w700.png"
